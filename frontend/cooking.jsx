3c63583d3865a0015c35be4f815060096cfa4e61
Anders Kindall <akindall47@gmail.com>
2022-05-16 11:33:58 -0700

    Add heroku

--- frontend/cooking.jsx
+++ frontend/cooking.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout, signup } from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Under Construction</h1>, root);
+  // window.login(user) = login;
+  // window.logout() = logout;
+  // window.signup(user) = signup;
+
 });
