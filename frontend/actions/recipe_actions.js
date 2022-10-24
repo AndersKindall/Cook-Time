3697787c33567f7c60a6b24e379b0f8b7f2c0059
Anders Kindall <akindall47@gmail.com>
2022-10-24 16:26:38 -0700

    Edit search function

--- frontend/actions/recipe_actions.js
+++ frontend/actions/recipe_actions.js
@@ -2,6 +2,10 @@ import * as RecipeAPIUtil from '../util/recipe_api_util'
 
 export const RECEIVE_RECIPE = 'RECEIVE_RECIPE';
 export const RECEIVE_ALL_RECIPES = 'RECEIVE_ALL_RECIPES';
+export const RECEIVE_SEARCH = 'RECEIVE_SEARCH';
+export const CLEAR_SEARCH = 'CLEAR_SEARCH';
+export const RECEIVE_SEARCH_PAGE = 'RECEIVE_SEARCH_PAGE';
+export const CLEAR_SEARCH_PAGE = 'CLEAR_SEARCH_PAGE';
 
 export const receiveRecipe = payload => ({
     type: RECEIVE_RECIPE,
@@ -21,4 +25,4 @@ export const getRecipe = recipeId => dispatch => (
 export const getAllRecipes = () => dispatch => (
     RecipeAPIUtil.fetchRecipes()
         .then(recipes => dispatch(receiveAllRecipes(recipes)))
-)
+);
