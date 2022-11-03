d38cb4990906e77c320fa46aad5981a409e232f0
Anders Kindall <akindall47@gmail.com>
2022-11-02 17:02:06 -0700

    Edit navbar

--- frontend/components/navbar/navbar_container.js
+++ frontend/components/navbar/navbar_container.js
@@ -1,18 +1,23 @@
 import { connect } from 'react-redux';
 
 import { logout } from '../../actions/session_actions';
-import { openModal } from '../../actions/modal_actions';
+import { closeModal, openModal } from '../../actions/modal_actions';
+import { search, clearSearch } from '../../actions/recipe_actions';
 import NavBar from './navbar';
 
 const mapStateToProps = ({ session }) => {
     return {
-        currentUser: session.currentUser
+        currentUser: session.currentUser,
+        searches: state.entities.search,
     };
 };
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(logout()),
-    openModal: modal => dispatch(openModal(modal))
+    openModal: modal => dispatch(openModal(modal)),
+    closeModal: () => dispatch(closeModal()),
+    search: (query) => dispatch(search(query)),
+    clearSearch: () => dispatch(clearSearch())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- frontend/components/search_recipes/search_recipes.jsx
+++ frontend/components/search_recipes/search_recipes.jsx
@@ -11,10 +11,40 @@ class SearchRecipes extends React.Component {
     }
 
     componentDidUpdate(oldProps) {
-        
+        if (this.props.location.pathname !== oldProps.location.pathname) {
+            this.props.clearSearchPage();
+            this.props.searchPage(this.props.match.params.query);
+            window.scrollTo(0, 0);
+        }
     }
-    render() {
 
+    render() {
+        let { currentUser, openModal, recipes, searches } = this.props
+        if (Object.values(recipes).length === 0) return (<div className="no-search-results"></div>);
+
+        if (searches.length === 0) return (
+            <div className='no-search-results'>
+                <h1 className="no-search-results-header">No recipe results for "{this.props.match.params.query}</h1>
+                <h1 className='no-search-results-subheader'>Search again for other recipes</h1>
+            </div>
+        );
+
+        return (
+            <div className='recipe-grid-outer-container' >
+                <div className='recipe-grid-container'>
+                    <h1 className='recipe-grid-title'>Search Results</h1> 
+                    <h2 className='recipe-grid-subtitle'>{searches.length} recipes</h2>
+                    <div className='recipe-grid'>
+                        {searches.map((search, idx) => {
+                            let searchId = search.id
+                            let recipe = recipes[searchId]
+                            return <RecipeIndexCard recipe={recipe} key={idx} currentUser={currentUser} openModal={openModal} />
+                        }
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
