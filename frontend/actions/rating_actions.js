d323bb54fe6f34813d806d6f3cc5c0542524f1b8
Anders Kindall <akindall47@gmail.com>
2022-12-07 20:17:14 -0800

    Add rating actions

---
+++ frontend/actions/rating_actions.js
@@ -0,0 +1,29 @@
+import * as RatingAPIUtil from "../util/rating_api_util";
+
+export const RECEIVE_RATING = "RECEIVE_RATING";
+export const DELETE_RATING = "DELETE_RATING";
+
+export const receiveRating = rating=> ({
+    type: RECEIVE_RATING,
+    rating
+})
+
+export const deleteRating = ratingId => ({
+    type: DELETE_RATING,
+    ratingId
+})
+
+export const addRating = (recipeId, ratingValue) => dispatch => (
+    RatingAPIUtil.postRating(recipeId, ratingValue)
+        .then(rating => dispatch(receiveRating(rating)))
+)
+
+export const updateThisRating = (ratingId, ratingValue) => dispatch (
+    RatingAPIUtil.updateRating(ratingId, ratingValue)
+        .then(rating => dispatch(receiveRating(rating)))
+)
+
+export const deleteThisRating = (ratingId) => dispatch => (
+    RatingAPIUtil.deleteRating(commentId)
+        .then(() => dispatch(deleteRating(ratingId)))
+)
