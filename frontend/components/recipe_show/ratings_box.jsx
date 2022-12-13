9eb80642efcda9cb7c5b32bb154036639d93b4bf
Anders Kindall <akindall47@gmail.com>
2022-12-12 17:19:33 -0800

    Build out RatingsBox class

--- frontend/components/recipe_show/ratings_box.jsx
+++ frontend/components/recipe_show/ratings_box.jsx
@@ -6,6 +6,32 @@ class RatingsBox extends React.Component {
         this.state = {
             ratingVal: 0
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.clearRating = this.clearRating.bind(this);
+        this.averageRating = this.averageRating.bind(this)
+        
+    }
+
+    handleChange(e) {
+       let newRatingVal = e.target.value;
+       this.setState({ ratingVal: newRatingVal}) 
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props(addRating(this.props.recipe.id, this.state.ratingVal))
+    }
+
+    clearRating(e) {
+        this.setState({ ratingVal: 0 })
+    }
+
+    averageRating(ratings) {
+        let total = 0;
+        ratings.map(rating => (total += rating.rating_value))
+        return Math.ceil(total/(ratings.length))
     }
 
     render() {
