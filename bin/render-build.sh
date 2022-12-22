620a81798a58565464923672424300deba18c5b2
Anders Kindall <akindall47@gmail.com>
2022-12-22 13:17:43 -0800

    Prepare to deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,11 @@
 # exit on error
 set -o errexit
 
-npm run build
 bundle install
-rails db:migrate
-rails db:seed #if needed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+#if site renders but no seeded data is present add this line 
+
+rails db:seed #if needed, comment this line out whenever pushing to a working deployment to avoid db errors
